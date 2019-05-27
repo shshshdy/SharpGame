32f4b4135c5d1789a6f68583255225fa73ca7e3f
bzh <381675273@qq.com>
2019-05-27 11:42:38 +0800

    file

--- Source/SharpGame.Base/FileSystem/File.cs
+++ Source/SharpGame.Base/FileSystem/File.cs
@@ -1,22 +1,29 @@
-﻿using System;
+using System;
 using System.IO;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace SharpGame
 {
-    public class File : NativeStreamWrapper
+    public class File : NativeStream
     {
         public StringID Name { get; set; }
+
+        protected Stream stream;
+        public Stream Stream => stream;
+
+        public File(Stream stream, int bufferSize = 4096)
+        {
+            this.stream = stream;
+            NativeStreamBufferSize = bufferSize;
+        }
 
         public bool IsEof
         {
             get { return stream.Position >= stream.Length; }
-        }
-
-        public File(Stream stream, int bufferSize = 4096) : base(stream, bufferSize)
-        {
         }
 
         public int Seek(int offset)
@@ -47,7 +54,7 @@ namespace SharpGame
 
             int sizeOfT = Unsafe.SizeOf<T>();
             int currentReadSize = stream.Read(nativeStreamBuffer, 0, sizeOfT);
-            if(currentReadSize != sizeOfT)
+            if (currentReadSize != sizeOfT)
                 throw new InvalidOperationException("Reached end of stream.");
 
             fixed (byte* temporaryBufferStart = nativeStreamBuffer)
@@ -63,10 +70,10 @@ namespace SharpGame
                 temporaryBuffer = nativeStreamBuffer = new byte[NativeStreamBufferSize];
 
             int count = 0;
-            while(!IsEof)
+            while (!IsEof)
             {
-                int c = stream.ReadByte();// nativeStreamBuffer, count, 1);
-                if(c == 0)
+                int c = stream.ReadByte();
+                if (c == 0)
                     break;
                 nativeStreamBuffer[count++] = (byte)c;
             }
@@ -77,12 +84,12 @@ namespace SharpGame
         public unsafe string ReadFileID()
         {
             byte* bytes = stackalloc byte[5];
-            if(4 != Read((IntPtr)bytes, 4))
+            if (4 != Read((IntPtr)bytes, 4))
             {
                 throw new InvalidOperationException("Reached end of stream.");
             }
 
-            bytes[4] = 0;                        
+            bytes[4] = 0;
             return Marshal.PtrToStringAnsi((IntPtr)bytes);
         }
 
@@ -90,15 +97,15 @@ namespace SharpGame
         {
             var result = new T[count];
 
-            IntPtr dest = Utilities.AsPointer(ref result[0]);            
-            int byteCount = count * Unsafe.SizeOf<T>();            
+            IntPtr dest = Utilities.AsPointer(ref result[0]);
+            int byteCount = count * Unsafe.SizeOf<T>();
             int currentReadSize = Read(dest, byteCount);
-            if(currentReadSize != byteCount)
+            if (currentReadSize != byteCount)
                 throw new InvalidOperationException("Reached end of stream.");
-            
+
             return result;
         }
-        
+
         public unsafe void Write<T>(ref T v) where T : struct
         {
             var temporaryBuffer = nativeStreamBuffer;
@@ -113,9 +120,154 @@ namespace SharpGame
 
         public unsafe void WriteArray<T>(T[] val) where T : struct
         {
-            IntPtr dest = Utilities.AsPointer(ref val[0]);            
+            IntPtr dest = Utilities.AsPointer(ref val[0]);
             int byteCount = val.Length * Unsafe.SizeOf<T>();
-            Write(dest, byteCount);   
+            Write(dest, byteCount);
+        }
+
+        /// <inheritdoc/>
+        public override Task CopyToAsync(Stream destination, int bufferSize, CancellationToken cancellationToken)
+        {
+            return stream.CopyToAsync(destination, bufferSize, cancellationToken);
+        }
+
+        /// <inheritdoc/>
+        public override void Flush()
+        {
+            stream.Flush();
+        }
+
+        /// <inheritdoc/>
+        public override Task FlushAsync(CancellationToken cancellationToken)
+        {
+            return stream.FlushAsync(cancellationToken);
+        }
+
+        /// <inheritdoc/>
+        public override Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
+        {
+            return stream.ReadAsync(buffer, offset, count, cancellationToken);
+        }
+
+        /// <inheritdoc/>
+        public override Task WriteAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
+        {
+            return stream.WriteAsync(buffer, offset, count, cancellationToken);
+        }
+
+        /// <inheritdoc/>
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            return stream.Seek(offset, origin);
+        }
+
+        /// <inheritdoc/>
+        public override void SetLength(long value)
+        {
+            stream.SetLength(value);
+        }
+
+        /// <inheritdoc/>
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            return stream.Read(buffer, offset, count);
+        }
+
+        /// <inheritdoc/>
+        public override int ReadByte()
+        {
+            return stream.ReadByte();
+        }
+
+        /// <inheritdoc/>
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            stream.Write(buffer, offset, count);
+        }
+
+        /// <inheritdoc/>
+        public override void WriteByte(byte value)
+        {
+            stream.WriteByte(value);
+        }
+
+        /// <inheritdoc/>
+        public override bool CanRead
+        {
+            get { return stream.CanRead; }
+        }
+
+        /// <inheritdoc/>
+        public override bool CanSeek
+        {
+            get { return stream.CanSeek; }
+        }
+
+        /// <inheritdoc/>
+        public override bool CanTimeout
+        {
+            get { return stream.CanTimeout; }
+        }
+
+        /// <inheritdoc/>
+        public override bool CanWrite
+        {
+            get { return stream.CanWrite; }
+        }
+
+        /// <inheritdoc/>
+        public override long Length
+        {
+            get { return stream.Length; }
+        }
+
+        /// <inheritdoc/>
+        public override long Position
+        {
+            get { return stream.Position; }
+            set { stream.Position = value; }
+        }
+
+        /// <inheritdoc/>
+        public override int ReadTimeout
+        {
+            get { return stream.ReadTimeout; }
+            set { stream.ReadTimeout = value; }
+        }
+
+        /// <inheritdoc/>
+        public override int WriteTimeout
+        {
+            get { return stream.WriteTimeout; }
+            set { stream.WriteTimeout = value; }
+        }
+
+        public override void Close()
+        {
+            try
+            {
+                stream.Close();
+            }
+            finally
+            {
+                base.Dispose(true);
+            }
+            
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            try
+            {
+                // Explicitly pick up a potentially methodimpl'ed Dispose
+                if (disposing)
+                    ((IDisposable)stream).Dispose();
+            }
+            finally
+            {
+                base.Dispose(disposing);
+            }
+            
         }
 
     }
