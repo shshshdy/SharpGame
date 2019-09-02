876ab387fbfa506577cf47ab89300bf2777a2573
bzh <381675273@qq.com>
2019-09-02 18:58:05 +0800

    glm

--- Source/SharpGame.Base/glm/mathext.cs
+++ Source/SharpGame.Base/glm/mathext.cs
@@ -15,6 +15,16 @@ namespace SharpGame
         {
             return new vec2(value.X, value.Y);
         }
+
+        public static implicit operator Vector2(vec2 value)
+        {
+            return new Vector2(value.x, value.y);
+        }
+
+        public static implicit operator vec2(Vector2 value)
+        {
+            return new vec2(value.X, value.Y);
+        }
     }
 
     public partial struct vec3
@@ -25,6 +35,16 @@ namespace SharpGame
         }
 
         public static explicit operator vec3(System.Numerics.Vector3 value)
+        {
+            return new vec3(value.X, value.Y, value.Z);
+        }
+
+        public static explicit operator Vector3(vec3 value)
+        {
+            return new Vector3(value.X, value.Y, value.Z);
+        }
+
+        public static explicit operator vec3(Vector3 value)
         {
             return new vec3(value.X, value.Y, value.Z);
         }
@@ -42,6 +62,15 @@ namespace SharpGame
             return new vec4(value.X, value.Y, value.Z, value.W);
         }
 
+        public static explicit operator Vector4(vec4 value)
+        {
+            return new Vector4(value.x, value.y, value.z, value.w);
+        }
+
+        public static explicit operator vec4(Vector4 value)
+        {
+            return new vec4(value.X, value.Y, value.Z, value.W);
+        }
     }
 
 }
