f3eb168a82aa9c2eb01c1f0f94d3e45a684e2fb2
BobbyBao <381675273@qq.com>
2020-10-24 11:31:09 +0800

    validation

--- Source/SharpGame.Graphics/Strings.cs
+++ Source/SharpGame.Graphics/Strings.cs
@@ -8,7 +8,7 @@
         public static UTF8String VK_KHR_XLIB_SURFACE_EXTENSION_NAME = "VK_KHR_xlib_surface";
         public static UTF8String VK_KHR_SWAPCHAIN_EXTENSION_NAME = "VK_KHR_swapchain";
         public static UTF8String VK_EXT_DEBUG_REPORT_EXTENSION_NAME = "VK_EXT_debug_report";
-        public static UTF8String StandardValidationLayeName = "VK_LAYER_LUNARG_standard_validation";
+        public static UTF8String StandardValidationLayeName = "VK_LAYER_KHRONOS_validation";// "VK_LAYER_LUNARG_standard_validation";
         public static UTF8String VK_DESCRIPTOR_BINDING_PARTIALLY_BOUND_BIT_EXT = "VK_DESCRIPTOR_BINDING_PARTIALLY_BOUND_BIT_EXT";
         public static UTF8String VK_KHR_GET_PHYSICAL_DEVICE_PROPERTIES_2_EXTENSION_NAME = "VK_KHR_get_physical_device_properties2";
         public static UTF8String VK_EXT_INLINE_UNIFORM_BLOCK_EXTENSION_NAME = "VK_EXT_inline_uniform_block";
